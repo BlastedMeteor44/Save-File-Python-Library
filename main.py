2033d9607320fb9ec1f4af7976b5b909b5c25932
BlastedMeteor44 <65218875+ruhaan-bit@users.noreply.github.com>
2024-05-05 10:43:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-def replace_line(filename, line_number, text):
-  with open(filename) as file:
-    lines = file.readlines()
-  if (line_number <= len(lines)):
-    lines[line_number - 1] = text + "\n"
-    with open(filename, "w") as file:
-      for line in lines:
-        file.write(line)
-  else:
-    print("Line", line_number, "not in file.")
-    print("File has", len(lines), "lines.")
-def read_line(fname, lnum):
-  try:
-    file = open(fname, "r")
-    lines = file.readlines()
-    file.close()
-  except:
-    return("Error reading line!") 
-  total_lines = len(lines)
-  if (lnum > total_lines):
-    return(str(total_lines) + " file lines.")
-    return("Can't read line " + str(lnum) + "!")
-  else:
-    line = lines[lnum - 1].rstrip('\n')
-    return(line)
+while 1 == 1:
+  def replace_line(filename, line_number, text):
+      with open(filename) as file:
+        lines = file.readlines()
+      if (line_number <= len(lines)):
+        lines[line_number - 1] = text + "\n"
+        with open(filename, "w") as file:
+          for line in lines:
+            file.write(line)
+      else:
+        print("Line", line_number, "not in file.")
+        print("File has", len(lines), "lines.")
+    def read_line(fname, lnum):
+      try:
+        file = open(fname, "r")
+        lines = file.readlines()
+        file.close()
+      except:
+        return("Error reading line!") 
+      total_lines = len(lines)
+      if (lnum > total_lines):
+        return(str(total_lines) + " file lines.")
+        return("Can't read line " + str(lnum) + "!")
+      else:
+        line = lines[lnum - 1].rstrip('\n')
+        return(line)
