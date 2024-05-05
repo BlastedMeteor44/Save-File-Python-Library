696ae22337decaa093bff67cc982a1dd66493c3e
BlastedMeteor44 <65218875+ruhaan-bit@users.noreply.github.com>
2024-05-05 09:38:22 +0800

    added main

---
+++ main.py
@@ -0,0 +1,26 @@
+def replace_line(filename, line_number, text):
+  with open(filename) as file:
+    lines = file.readlines()
+  if (line_number <= len(lines)):
+    lines[line_number - 1] = text + "\n"
+    with open(filename, "w") as file:
+      for line in lines:
+        file.write(line)
+  else:
+    print("Line", line_number, "not in file.")
+    print("File has", len(lines), "lines.")
+def read_line(fname, lnum):
+  try:
+    file = open(fname, "r")
+    lines = file.readlines()
+    file.close()
+  except:
+    print("Error reading file.")
+    return 
+  total_lines = len(lines)
+  if (lnum > total_lines):
+    print(str(total_lines) + " file lines.")
+    print("Can't read line " + str(lnum) + "!")
+  else:
+    line = lines[lnum - 1].rstrip('\n')
+    return(line)
